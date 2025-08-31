5abb95c7bb2fce8d4c21a9797ed055a25a936733
sunil-gumatimath <sunilgumatimath.vs@gmail.com>
2025-08-31 22:02:43 +0530

    useState Example on Counter program

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import CarExample from './useState Examples/CarExample'
 import ColorChnager from './useState Examples/ColorChanger'
+import Counter from './useState Examples/Counter'
 function App() {
 
   return (
     <>
       {/* <ColorChnager />*/}
-      <CarExample />
+      {/* <CarExample />*/}
+      <Counter />
     </>
   )
 }
---
+++ src/useState Examples/Counter.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function Counter() {
+
+  const [count, setCount] = useState(0)
+
+  const incrementCount = () => {
+    setCount(count => count + 1)
+  }
+
+  const decrementCount = () => {
+    setCount(count => count - 1)
+  }
+
+  const resetCount = () => {
+    setCount(0)
+  }
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={decrementCount}>Decrement</button>
+      <button onClick={resetCount}>Reset</button>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+
+export default Counter;
